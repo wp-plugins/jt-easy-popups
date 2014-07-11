bf5aaded3b2e4a7e169134a8a4964e6e6596f64b
cheolguso <cheolguso@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-11 07:33:20 +0000

    Renewal is in preparation.
    git-svn-id: https://plugins.svn.wordpress.org/jt-easy-popups/trunk@946752 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jt-easy-popups.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Plugin Name: JT Easy Popups
+ * Plugin URI: http://www.studio-jt.co.kr
+ * Description: 간단한 이미지 팝업에 기간을 설정하여 누구나 쉽게 사용할 수 있습니다.
+ * Version: 1.0.0
+ * Author: 스튜디오 제이티 (support@studio-jt.co.kr)
+ * Author URI: studio-jt.co.kr
+*/
+?>
